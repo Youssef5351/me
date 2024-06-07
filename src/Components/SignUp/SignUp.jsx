0c147f75626e159f21ae2d4217e3b239c6032acc
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-06-07 09:51:41 +0300

    Update SignUp.jsx

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -60,7 +60,7 @@ const SignUp = () => {
         }
 
         try {
-            const response = await axios.post('me1-p3t7i6tgb-beauty-tteam.vercel.app', formData);
+            const response = await axios.post('me1-p3t7i6tgb-beauty-tteam.vercel.app/api/signup', formData);
             if (response.data.success) {
                 setSuccess('ok');
                 setError('');
